56714ab9d2644be6319c76c6021f5bd50f972281
iamsaimsheikh <iamsaimsheikh@gmail.com>
2020-11-27 18:47:01 +0500

    task 2

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { preventAutoHide } from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
+  const [original, nextOriginal] = React.useState('Original');
+  const [discount, nextDiscount] = React.useState('Discount %');
+
+
   return (
     <View style={styles.container}>
-      <Text>Discount</Text>
-      <StatusBar style="auto" />
+      <View style={styles.headingView}>
+      <Text style={styles.heading}>DISCOUNT</Text>
+      </View>
+    <View style={styles.txtDiv}>
+
+      <TextInput
+      style={styles.txtInput}
+      onChangeText={text => nextOriginal(text)}
+      value={original}
+      keyboardType={'number-pad'}
+    />
+      <TextInput
+      style={styles.txtInput}
+      onChangeText={text => nextDiscount(text)}
+      value={discount}
+      keyboardType={'number-pad'}
+      
+    />
+    </View>
     </View>
   );
 }
@@ -15,12 +36,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
+
+  headingView :{
+    flex:0.3,
+    marginTop:10,
+    alignItems:'flex-end',
+    justifyContent:'flex-end'
+  },  
+
   heading:{
-    marginLeft: auto,
-    marginRight:auto,
+    marginLeft: "auto",
+    marginRight:"auto",
+    padding:20,
+    fontSize:30,
+    color:"white"
+  },
+
+  txtDiv:{
+    flex:0.5,
+    alignItems: 'flex-start',
+    justifyContent: "flex-end",
+    flexDirection:'row'
+  },
+
+  txtInput:{
+    height: 40,
+    borderColor: 'gray', 
+    borderWidth: 1,
+    width:150,
+    backgroundColor:'white',
+    margin:10,
+    textAlign:'center'
   }
 });
